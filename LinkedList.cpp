f4dbe6cf369bb9780bcd8493a5d26f29a804db93
Jack <jacksdggn@gmail.com>
2024-04-28 21:54:31 +0930

    - LinkedList insert and delete position

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,7 +4,6 @@
 LinkedList::LinkedList() {
   head = nullptr;
 }
-
 LinkedList::LinkedList(int* array, int len) {
   Node* temp = new Node(array[len-1]);
 
@@ -35,4 +34,24 @@ void LinkedList::printList() {
     }
   }
   std::cout << "]" << std::endl;
+}
+
+void LinkedList::insertPosition(int pos, int newNum) {
+  Node* temp = head;
+  while (pos > 2 && temp != nullptr) { // -2 because indexing starts at 1
+    temp = temp->getLink();
+    pos--;
+  }
+  temp->setLink(new Node(newNum, temp->getLink()));
+}
+
+bool LinkedList::deletePosition(int pos) {
+  Node* temp = head;
+  while (pos > 2 && temp != nullptr) {
+    temp = temp->getLink();
+    pos--;
+  }
+  if (temp == nullptr)
+    return false;
+  temp->setLink(temp->getLink()->getLink());
 }
--- main.cpp
+++ main.cpp
@@ -5,4 +5,10 @@ int main() {
   int x[5] = {1, 2, 3, 4, 5};
   LinkedList list(x, 5);
   list.printList();
+  list.insertPosition(3, 0);
+  list.printList();
+  bool z = list.deletePosition(3);
+  if (z) list.printList();
+  bool y = list.deletePosition(7);
+  if (y) list.printList();
 }
